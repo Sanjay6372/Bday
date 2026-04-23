88eb92aa2af2f8eee9b0672cde7fd6a73fae3796
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 23:50:27 +0530

    Update mic.js

--- js/mic.js
+++ js/mic.js
@@ -1,11 +1,3 @@
 function startMic() {
-  document.getElementById("status").innerText = "Blowing... 💨";
-
-  setTimeout(() => {
-    document.querySelectorAll(".flame").forEach(f => {
-      f.style.display = "none";
-    });
-
-    document.getElementById("status").innerText = "Candles blown ❤️";
-  }, 2000);
+  alert("Blow detected 🎤 (Mic feature simplified for now)");
 }
