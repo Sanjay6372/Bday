8daf68b1cd7dbf07e07c6f78b62dd74d27c12dd7
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 23:05:57 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,26 @@ function nextPage(page) {
 function cutCake() {
   document.getElementById("cake").classList.add("cut");
 }
+
+/* HEARTS EFFECT */
+function createHearts() {
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.innerHTML = "❤️";
+    heart.className = "heart";
+
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (12 + Math.random() * 10) + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 6000);
+  }, 500);
+}
+
+/* START */
+window.onload = () => {
+  createHearts();
+};
