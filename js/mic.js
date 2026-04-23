153da230ce6a5f6dd4ef548f6821cdef30695e5f
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 17:25:09 +0530

    Update mic.js

--- js/mic.js
+++ js/mic.js
@@ -1,22 +1,28 @@
-navigator.mediaDevices.getUserMedia({ audio: true })
-.then(stream => {
-  const ctx = new AudioContext();
-  const mic = ctx.createMediaStreamSource(stream);
-  const analyser = ctx.createAnalyser();
+function startMic() {
+  document.getElementById("micStatus").innerText = "Listening... blow now 💨";
 
-  mic.connect(analyser);
-  const data = new Uint8Array(analyser.frequencyBinCount);
+  navigator.mediaDevices.getUserMedia({ audio: true })
+  .then(stream => {
+    const ctx = new AudioContext();
+    const mic = ctx.createMediaStreamSource(stream);
+    const analyser = ctx.createAnalyser();
 
-  function detect() {
-    analyser.getByteFrequencyData(data);
-    let volume = data.reduce((a,b)=>a+b)/data.length;
+    mic.connect(analyser);
 
-    if(volume > 60){
-      document.querySelectorAll(".flame").forEach(f => f.style.display="none");
+    const data = new Uint8Array(analyser.frequencyBinCount);
+
+    function detect() {
+      analyser.getByteFrequencyData(data);
+      let volume = data.reduce((a,b)=>a+b)/data.length;
+
+      if(volume > 60){
+        document.querySelectorAll(".flame").forEach(f=>f.style.display="none");
+        document.getElementById("micStatus").innerText = "🎉 Candles blown!";
+      }
+
+      requestAnimationFrame(detect);
     }
 
-    requestAnimationFrame(detect);
-  }
-
-  detect();
-});
+    detect();
+  });
+}
