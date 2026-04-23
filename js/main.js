f3eee68595a56da97850cc3782cb6a6205bbe560
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 22:41:50 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ function nextPage(page) {
   window.location.href = page;
 }
 
-window.onload = () => {
-  document.body.classList.add("loaded");
-
-  const music = document.getElementById("music");
-  if (music) {
-    document.body.addEventListener("click", () => {
-      music.play();
-    }, { once: true });
-  }
-};
+function cutCake() {
+  document.getElementById("cake").classList.add("cut");
+}
