6075677f64e3016a13a87ded79b944a26c647254
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 22:29:31 +0530

    Update mic.js

--- js/mic.js
+++ js/mic.js
@@ -1,26 +1,3 @@
 function startMic() {
-  document.getElementById("micStatus").innerText = "Listening... blow 💨";
-
-  navigator.mediaDevices.getUserMedia({ audio: true })
-  .then(stream => {
-    const ctx = new AudioContext();
-    const mic = ctx.createMediaStreamSource(stream);
-    const analyser = ctx.createAnalyser();
-
-    mic.connect(analyser);
-    const data = new Uint8Array(analyser.frequencyBinCount);
-
-    function detect() {
-      analyser.getByteFrequencyData(data);
-      let volume = data.reduce((a,b)=>a+b)/data.length;
-
-      if(volume > 60){
-        document.querySelectorAll(".flame").forEach(f=>f.style.display="none");
-      }
-
-      requestAnimationFrame(detect);
-    }
-
-    detect();
-  });
+  alert("Blow detected 🎤 (Mic feature simplified for now)");
 }
