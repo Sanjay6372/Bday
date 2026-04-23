1a18e502876b1f5367ebc7945fe05707dd777733
Sanjay6372 <114331878+Sanjay6372@users.noreply.github.com>
2026-04-23 17:24:53 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,9 +3,19 @@ function nextPage(page) {
 }
 
 function startMusic(page) {
-  const music = document.getElementById("music");
-  if (music) {
-    music.play().catch(()=>{});
-  }
+  let m = document.getElementById("music");
+  if(m) m.play().catch(()=>{});
   window.location.href = page;
+}
+
+function createHearts() {
+  for (let i = 0; i < 12; i++) {
+    let h = document.createElement("div");
+    h.innerHTML = "❤️";
+    h.className = "heart";
+    h.style.left = Math.random()*100+"vw";
+    h.style.fontSize = (Math.random()*20+10)+"px";
+    h.style.animationDuration = (Math.random()*3+3)+"s";
+    document.body.appendChild(h);
+  }
 }
